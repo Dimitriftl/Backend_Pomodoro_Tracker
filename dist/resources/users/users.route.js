9ce06aa591c1a1178336e19d0195524e5d48766a
Dimitri Fontenelle <dimitriftl@hotmail.com>
2024-01-23 18:09:54 +0100

    :sparkles: connect to right DB and handle create user

--- dist/resources/users/users.route.js
+++ dist/resources/users/users.route.js
@@ -2,16 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const { validateUser } = require("../middlewares/validateUser");
-const { signUp, getAllUsers, logIn, deleteUser, getUser, updateUser, } = require("./users.controller");
+const { signUp, logIn, deleteUser, getUser, updateUser, } = require("./users.controller");
 const { authentification } = require("../middlewares/authentification");
 const router = express.Router();
 // // middleware validate user
-// router.route("").get(authentification, getAllUsers);
-// router.route("/signup").post(validateUser, signUp);
-// router.route("/login").post(logIn);
-// router
-//   .route("/user")
-//   .delete(authentification, deleteUser)
-//   .get(authentification, getUser)
-//   .put(authentification, updateUser);
+router.route("/signup").post(validateUser, signUp);
+router.route("/login").post(logIn);
+router.route("/").get((req, res) => {
+    res.send("Hello World!");
+}); // test route
+router
+    .route("/user")
+    .delete(authentification, deleteUser)
+    .get(authentification, getUser)
+    .put(authentification, updateUser);
 module.exports = router;
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = async () => {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mzvon6w.mongodb.net/?retryWrites=true&w=majority`;
-    try {
-        await mongoose.connect(uri);
-        console.log('Connected to DB');
-    } catch (error) {
-        console.log(error);
-    }
-}; 
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mzvon6w.mongodb.net/pomodoro_tracker?retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(uri, {
+      dbName: "Pomodoro_tracker",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
